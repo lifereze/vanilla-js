9c81f482e7daf476c4f23ae73a0767976152b77f
lifereze <liferezearnold@gmail.com>
2020-06-05 00:19:15 +0300

    add a function 'addtoDo' to gather input from the user interphase

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
-const todoInput = document.querySelector('.todo-list');
+const todoList = document.querySelector('.todo-list');
 
 // eventListeners
-
+todoButton.addEventListener('click', addToDo);
 
 // functions
+function addToDo(event){
+    // prevent form from submitting
+    event.preventDefault();
+    console.log("hello") ;
+}
