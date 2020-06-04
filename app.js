ecac85542e0dc1ecff603d55e3c0ebd18f4ceb21
lifereze <liferezearnold@gmail.com>
2020-06-05 00:08:09 +0300

    add the query selectors targeting the html

---
+++ app.js
@@ -0,0 +1,9 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoInput = document.querySelector('.todo-list');
+
+// eventListeners
+
+
+// functions
